862646df09f659f5a5d0c9de882a932b73e81291
Noah Sanches <167345061+noahsanches10@users.noreply.github.com>
2025-12-24 07:18:56 +0000

    minor fixes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Allow production builds even if TypeScript errors are present
     ignoreBuildErrors: true,
